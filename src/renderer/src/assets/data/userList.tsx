afc4ca7ceb9b233840e33e218088dcba427e96a0
Yashrif Arifin <yaswoccho@gmail.com>
2023-11-17 23:34:54 +0600

    feat: add student list title

--- src/renderer/src/assets/data/userList.tsx
+++ src/renderer/src/assets/data/userList.tsx
@@ -2,6 +2,7 @@ import { IoTrashOutline } from 'react-icons/io5'
 
 import { User } from '@renderer/types/user'
 
+export const data = { title: 'Student List' }
 export const button = {
   remove: {
     title: 'Remove Student',
--- src/renderer/src/components/UserList.tsx
+++ src/renderer/src/components/UserList.tsx
@@ -1,6 +1,6 @@
 import { FC, useContext } from 'react'
 
-import { button } from '@renderer/assets/data/userList'
+import { button, data } from '@renderer/assets/data/userList'
 import { ImageContext } from '@renderer/context/ImageContext'
 import { UserContext } from '@renderer/context/UserContext'
 import { ImageContextType } from '@renderer/types/image'
@@ -17,7 +17,7 @@ const UserList = () => {
     const { name, studentId, image } = user
 
     return (
-      <div className="flex items-center justify-between gap-24 w-full max-w-3xl">
+      <div className="w-full flex items-center justify-between gap-24">
         <div className="flex items-center gap-6 px-6 py-4 rounded-lg">
           <img src={image} className="w-12 h-12 rounded-full" />
           <div>
@@ -36,13 +36,18 @@ const UserList = () => {
   }
 
   return (
-    <div className="flex flex-col items-center divide-y-[1.5px] overflow-scroll">
-      {users &&
-        images &&
-        Object.values(users).map(({ id, name, studentId }) => {
-          const image = images[id] ? URL.createObjectURL(images[id][0].blob) : undefined
-          return <UserCard key={id} id={id} name={name} image={image} studentId={studentId} />
-        })}
+    <div className="flex flex-col items-center gap-8 mt-6">
+      <h1 className="text-3xl pb-1 font-semibold text-light-blue-b3 text-center border-b-2 border-light-blue-ff/50">
+        {data.title}
+      </h1>
+      <div className="max-w-3xl w-full flex flex-col items-center divide-y-[1.5px] overflow-scroll">
+        {users &&
+          images &&
+          Object.values(users).map(({ id, name, studentId }) => {
+            const image = images[id] ? URL.createObjectURL(images[id][0].blob) : undefined
+            return <UserCard key={id} id={id} name={name} image={image} studentId={studentId} />
+          })}
+      </div>
     </div>
   )
 }
